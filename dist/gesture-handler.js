def70e08cfd5b1dcf626479f7cccf37402ad9a67
Shawn Yu <shawnyudesign@gmail.com>
2022-09-05 15:51:36 -0400

    sdsdsd

--- dist/gesture-handler.js
+++ dist/gesture-handler.js
@@ -12,16 +12,16 @@ AFRAME.registerComponent("gesture-handler", {
     this.handleScale = this.handleScale.bind(this);
     this.handleRotation = this.handleRotation.bind(this);
 
-    this.isVisible = false;
+    this.isblock = false;
     this.initialScale = this.el.object3D.scale.clone();
     this.scaleFactor = 1;
 
     this.el.sceneEl.addEventListener("markerFound", (e) => {
-      this.isVisible = true;
+      this.isblock = true;
     });
 
     this.el.sceneEl.addEventListener("markerLost", (e) => {
-      this.isVisible = false;
+      this.isblock = false;
     });
   },
 
@@ -55,9 +55,23 @@ if(this.creatureThrottle ==="off"){
     switch(creatureType){
       case "whale": creatureType = "bird";
       $("#creature").attr("position",".3 1 0" )
+      $(".name").css("display", "none")
+      $(".commonname").css("display", "none")
+      $(".habitat").css("display", "none")
+      $(".additional").css("display", "none")  
       setTimeout(() => {
+        $("#creature").attr("position","0 1 0" )
+        $(".name").css("display", "block")
+        $(".commonname").css("display", "block")
+        $(".habitat").css("display", "block")
+        $(".additional").css("display", "block")
         $("#creature").attr("gltf-model", "./assets/bird.glb")
         $("#creature").attr("position","0 1 0" )
+        $(".name").html(creatureInformation.bird.name)
+        $(".commonname").html(creatureInformation.bird.commonName)
+        $(".habitat").html(creatureInformation.bird.habitat)
+        $(".additional").html(creatureInformation.bird.additional)
+
       }, 100);
       
       this.creatureThrottle = "on"
@@ -65,20 +79,55 @@ if(this.creatureThrottle ==="off"){
 
       case "bird": creatureType = "bull";
       $("#creature").attr("position",".3 1 0" )
+      $("#creature").attr("position",".3 1 0" )
+      $(".name").html("display", "none")
+      $(".commonname").css("display", "none")
+      $(".habitat").css("display", "none")
+      $(".additional").css("display", "none")        
+   
       setTimeout(() => {
         $("#creature").attr("gltf-model", "./assets/bull.glb")
+        $("#creature").attr("rotation","0 180 0" )
+
         $("#creature").attr("position","0 1 0" )
+        $("#creature").attr("position","0 1 0" )
+        $(".name").css("display", "block")
+        $(".commonname").css("display", "block")
+        $(".habitat").css("display", "block")
+        $(".additional").css("display", "block")
+        $(".name").html(creatureInformation.bull.name)
+        $(".commonname").html(creatureInformation.bull.commonName)
+        $(".habitat").html(creatureInformation.bull.habitat)
+        $(".additional").html(creatureInformation.bull.additional)
+
       }, 100);
       this.creatureThrottle = "on"
       break;
 
       case "bull": creatureType = "whale";
       $("#creature").attr("position",".3 1 0" )
+      $(".name").html("display", "none")
+      $(".commonname").css("display", "none")
+      $(".habitat").css("display", "none")
+      $(".additional").css("display", "none")        
+   
       setTimeout(() => {
         $("#creature").attr("gltf-model", "./assets/whale.glb")
+        $("#creature").attr("rotation","0 0 0" )
+
         $("#creature").attr("position","0 1 0" )
+        $(".name").html("display", "block")
+        $(".commonname").css("display", "block")
+        $(".habitat").css("display", "block")
+        $(".additional").css("display", "block")
+
+
+        $(".name").html(creatureInformation.whale.name)
+        $(".commonname").html(creatureInformation.whale.commonName)
+        $(".habitat").html(creatureInformation.whale.habitat)
+        $(".additional").html(creatureInformation.whale.additional)
       }, 100);
-      this.creatureThrottle = "on"
+      this.creatureThrottle = "on" 
       
      }
 
@@ -91,7 +140,7 @@ if(this.creatureThrottle ==="off"){
   },
 
   handleRotation: function (event) {
-    if (this.isVisible) {
+    if (this.isblock) {
       this.el.object3D.rotation.y +=
         event.detail.positionChange.x * this.data.rotationFactor;
       this.el.object3D.rotation.x +=
@@ -100,7 +149,7 @@ if(this.creatureThrottle ==="off"){
   },
 
   handleScale: function (event) {
-    if (this.isVisible) {
+    if (this.isblock) {
       this.scaleFactor *=
         1 + event.detail.spreadChange / event.detail.startSpread;
 
